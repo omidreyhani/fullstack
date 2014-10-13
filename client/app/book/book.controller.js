7efa31e27390f73e794049ea80c4c2cfeef3a231
Omid reyhani <omid.reyhani@gmail.com>
2014-10-13 07:06:42 +0300

    l

---
+++ client/app/book/book.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('fullstackApp')
+  .controller('BookCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/book/book.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('fullstackApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('book', {
+        url: '/book',
+        templateUrl: 'app/book/book.html',
+        controller: 'BookCtrl'
+      });
+  });
